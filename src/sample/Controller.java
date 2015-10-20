47bd62c952b633d596f80c067c454b9b15e84dee
JackNel <jcneligan@gmail.com>
2015-10-20 14:00:26 -0400

    AddContact method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField phoneField;
+    @FXML
+    TextField emailField;
+    @FXML
+    ListView listView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {listView.setItems(contacts);}
+
+    public void addContact() {
+        Contact contact = new Contact(nameField.getText(),phoneField.getText(), emailField.getText());
+        contacts.add(contact);
+        nameField.setText("");
+        phoneField.setText("");
+        emailField.setText("");
+    }
+
+    public void removeContact() {
+
+    }
+
+
 }
